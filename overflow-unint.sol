871a513d5d536fad7085ed14ba91478eb92cb14a
Farmer <1299076979@qq.com>
2024-05-23 19:00:11 +0800

    Update overflow-unint.sol

--- overflow-unint.sol
+++ overflow-unint.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.6.0;
 
 
 contract SimpleTokenTest {
+
+    //uint256 cant assign negative value . uint256 become big value when encounter negative operation 
     uint256 public  totalSupply;
 
     uint256 public  test;
 
 
+
     function subtractUint(uint256 s1,uint256 s2) public returns(uint256)  {
         totalSupply= s1-s2;
-
+            
         return totalSupply;
     }
 
