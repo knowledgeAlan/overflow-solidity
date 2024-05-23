223e5f79f84c8f8e19da56fad19db75db23abc74
Farmer <1299076979@qq.com>
2024-05-23 18:58:42 +0800

    Create overflow-unint.sol

---
+++ overflow-unint.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+
+contract SimpleTokenTest {
+    uint256 public  totalSupply;
+
+    uint256 public  test;
+
+
+    function subtractUint(uint256 s1,uint256 s2) public returns(uint256)  {
+        totalSupply= s1-s2;
+
+        return totalSupply;
+    }
+
+     
+}
